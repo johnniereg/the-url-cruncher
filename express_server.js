782b1abf625068d6e95eecb271e04b3f8fe56908
Johnnie Regalado <johnniereg@gmail.com>
2017-11-07 22:21:03 +0000

    Installed body-parser and connected through server

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,6 +12,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Index with just a welcome message.
 app.get("/", (request, response) => {
   response.end("Welcome to The Link Cruncher!");
 });
@@ -26,14 +30,18 @@ app.get("/urls/:id", (request, response) => {
   response.render("urls_show", templateVars);
 });
 
+// Gives JSON of the URL Database
 app.get("/urls.json", (request, response) => {
   response.json(urlDatabase);
 });
 
+// app.get test for page /hello
 app.get("/hello", (request, response) => {
   response.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+// Turns on server and listens at specified PORT.
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
